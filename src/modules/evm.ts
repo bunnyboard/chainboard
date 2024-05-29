f8e620ddb2ecb3297e6e7be2aa5fa82b58b3dff8
Noat Eden <noat.eth@gmail.com>
2024-05-30 00:29:27 +0700

    try catch on failed query rpc

--- src/modules/evm.ts
+++ src/modules/evm.ts
@@ -54,101 +54,103 @@ export default class EvmChainAdapter extends ChainAdapter {
     }
 
     for (const nodeRpc of this.chainConfig.nodeRpcs) {
-      const client = this.getPublicClient(nodeRpc);
-      const rawBlock = await getBlock(client, {
-        blockNumber: BigInt(blockNumber),
-        includeTransactions: true,
-      });
-      const logs = await client.getLogs({
-        fromBlock: BigInt(blockNumber),
-        toBlock: BigInt(blockNumber),
-      });
-
-      if (rawBlock) {
-        const blockData: BlockData = {
-          chain: this.chainConfig.name,
-          family: this.chainConfig.family,
-          number: Number(rawBlock.number),
-          timestamp: Number(rawBlock.timestamp),
-
-          gasUsed: Number(rawBlock.gasUsed),
-          gasLimit: Number(rawBlock.gasLimit),
-
-          totalCoinTransfer: '0',
-
-          // eip 1559
-          totalCoinBurnt: this.chainConfig.eip1559 ? '0' : undefined,
-
-          deployedContracts: 0,
-
-          transactions: rawBlock.transactions.length,
-
-          fromAddresses: [],
-          toAddresses: [],
-
-          contractLogs: [],
-        };
-
-        const fromAddresses: any = {};
-        const toAddresses: any = {};
-        const transactionDeployContracts: any = {};
-
-        for (const transaction of rawBlock.transactions) {
-          if (transaction.gasPrice && transaction.gasPrice === 0n) {
-            // ignore, layer 2 system transaction
-            continue;
-          }
-
-          // count unique addresses
-          fromAddresses[normalizeAddress(transaction.from)] = true;
-          if (transaction.to) {
-            toAddresses[normalizeAddress(transaction.to)] = true;
-          }
-
-          if (!transaction.to && transaction.input !== '0x0' && (transaction.input as string) !== '') {
-            transactionDeployContracts[transaction.hash] = true;
-          }
-
-          // count coin volume transfer
-          blockData.totalCoinTransfer = new BigNumber(blockData.totalCoinTransfer)
-            .plus(new BigNumber(transaction.value.toString()).dividedBy(1e18))
-            .toString(10);
-        }
-
-        // count coin were burnt if any, EIP-1559
-        // coin burnt = baseFeePerGas * gasUsed
-        if (rawBlock.baseFeePerGas && blockData.totalCoinBurnt && this.chainConfig.eip1559) {
-          blockData.totalCoinBurnt = new BigNumber(blockData.totalCoinBurnt)
-            .plus(
-              new BigNumber(rawBlock.baseFeePerGas.toString())
-                .multipliedBy(rawBlock.gasUsed.toString())
-                .dividedBy(1e18),
-            )
-            .toString(10);
-        }
-
-        blockData.deployedContracts = Object.keys(transactionDeployContracts).length;
-
-        blockData.fromAddresses = Object.keys(fromAddresses);
-        blockData.toAddresses = Object.keys(toAddresses);
-
-        blockData.contractLogs = logs.map((item) => {
-          return {
-            address: normalizeAddress(item.address),
-            signature: item.topics[0] ? item.topics[0] : '',
-          };
+      try {
+        const client = this.getPublicClient(nodeRpc);
+        const rawBlock = await getBlock(client, {
+          blockNumber: BigInt(blockNumber),
+          includeTransactions: true,
+        });
+        const logs = await client.getLogs({
+          fromBlock: BigInt(blockNumber),
+          toBlock: BigInt(blockNumber),
         });
 
-        return blockData;
+        if (rawBlock) {
+          const blockData: BlockData = {
+            chain: this.chainConfig.name,
+            family: this.chainConfig.family,
+            number: Number(rawBlock.number),
+            timestamp: Number(rawBlock.timestamp),
+
+            gasUsed: Number(rawBlock.gasUsed),
+            gasLimit: Number(rawBlock.gasLimit),
+
+            totalCoinTransfer: '0',
+
+            // eip 1559
+            totalCoinBurnt: this.chainConfig.eip1559 ? '0' : undefined,
+
+            deployedContracts: 0,
+
+            transactions: rawBlock.transactions.length,
+
+            fromAddresses: [],
+            toAddresses: [],
+
+            contractLogs: [],
+          };
+
+          const fromAddresses: any = {};
+          const toAddresses: any = {};
+          const transactionDeployContracts: any = {};
+
+          for (const transaction of rawBlock.transactions) {
+            if (transaction.gasPrice && transaction.gasPrice === 0n) {
+              // ignore, layer 2 system transaction
+              continue;
+            }
+
+            // count unique addresses
+            fromAddresses[normalizeAddress(transaction.from)] = true;
+            if (transaction.to) {
+              toAddresses[normalizeAddress(transaction.to)] = true;
+            }
+
+            if (!transaction.to && transaction.input !== '0x0' && (transaction.input as string) !== '') {
+              transactionDeployContracts[transaction.hash] = true;
+            }
+
+            // count coin volume transfer
+            blockData.totalCoinTransfer = new BigNumber(blockData.totalCoinTransfer)
+              .plus(new BigNumber(transaction.value.toString()).dividedBy(1e18))
+              .toString(10);
+          }
+
+          // count coin were burnt if any, EIP-1559
+          // coin burnt = baseFeePerGas * gasUsed
+          if (rawBlock.baseFeePerGas && blockData.totalCoinBurnt && this.chainConfig.eip1559) {
+            blockData.totalCoinBurnt = new BigNumber(blockData.totalCoinBurnt)
+              .plus(
+                new BigNumber(rawBlock.baseFeePerGas.toString())
+                  .multipliedBy(rawBlock.gasUsed.toString())
+                  .dividedBy(1e18),
+              )
+              .toString(10);
+          }
+
+          blockData.deployedContracts = Object.keys(transactionDeployContracts).length;
+
+          blockData.fromAddresses = Object.keys(fromAddresses);
+          blockData.toAddresses = Object.keys(toAddresses);
+
+          blockData.contractLogs = logs.map((item) => {
+            return {
+              address: normalizeAddress(item.address),
+              signature: item.topics[0] ? item.topics[0] : '',
+            };
+          });
+
+          return blockData;
+        }
+      } catch (e: any) {
+        logger.warn('failed to get data from rpc', {
+          service: this.name,
+          chain: this.chainConfig.name,
+          rpc: nodeRpc,
+        });
+        console.log(e);
+        await sleep(1);
       }
-
-      logger.warn('failed to get block data from rpc', {
-        service: this.name,
-        chain: this.chainConfig.name,
-        rpc: nodeRpc,
-      });
-
-      await sleep(1);
     }
 
     return null;
